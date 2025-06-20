c453708550cff06e9204d38363fb359e8a50eb88
j-peace <tiagopaz.hs@gmail.com>
2025-06-20 17:24:57 -0300

    Initial Commit

---
+++ TemperatureConversor/TemperatureConversorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversorApp.swift
+//  TemperatureConversor
+//
+//  Created by Suno on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
