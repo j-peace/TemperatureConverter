6e6c0f3201b8eed7256db0f4b55bb3a64e3f5fdf
j-peace <tiagopaz.hs@gmail.com>
2025-06-22 11:29:46 -0300

    finishing home screen

--- TemperatureConversor/ContentView.swift
+++ TemperatureConversor/ContentView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct ContentView: View {
     private let temperatureInCelsius: Double = 17.0
+    @State private var isTextHidden = false
+
     var body: some View {
         ZStack{
             LinearGradient(
@@ -27,24 +29,101 @@ struct ContentView: View {
                 )
                 .font(.custom("GillSans-SemiBold", size: 20))
                 .foregroundColor(Color(red: 40/255, green: 47/255, blue: 67/255))
-
+                .padding(.bottom, 20)
+                
                 ZStack {
-                    Color(red: 42/255, green: 42/255, blue: 44/255)
-                        .frame(width: 202, height: 232)
-                        .padding(.trailing, 10)
+                    if !isTextHidden {
+                        Color(red: 42/255, green: 42/255, blue: 44/255)
+                            .frame(width: 202, height: 232)
+                            .padding(.trailing, 10)
+                    }else {
+                        Color(red: 117/255, green: 118/255, blue: 135/255)
+                            .frame(width: 202, height: 232)
+                            .padding(.trailing, 10)
+                    }
                     
-                    // Hide when press button
                     Image("mockup-apple-watch")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 500, height: 500)
                     
-                    // Shiw when press button
-                    Text("\(temperatureInCelsius, specifier: "%.0f") °C")
-                        .font(.largeTitle)
-                        .foregroundColor(.black)
+                    if isTextHidden {
+                        
+                        VStack {
+                            
+                            Image("temperaturas")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .padding()
+                            
+                            Text("\(temperatureInCelsius, specifier: "%.0f") °C")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+
+                            HStack (spacing: 10) {
+                                VStack {
+                                    Image(systemName: "chevron.down")
+                                        .resizable()
+                                        .frame(width: 15, height: 8)
+                                    Image(systemName: "chevron.down")
+                                        .resizable()
+                                        .frame(width: 15, height: 8)
+                                        .padding(.top, -7)
+                                }
+                                Text("swipe to change")
+                                    .font(.custom("GillSans-SemiBold", size: 17))
+                                    .foregroundColor(Color(red: 40/255, green: 47/255, blue: 67/255))
+                            }
+                            .padding(.top, 18)
+                        }
+                    }
                     
-                }
+                    if !isTextHidden {
+                        VStack {
+                            
+                            Image("temperaturas")
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                                .padding()
+
+                            Button(action: {
+                                isTextHidden.toggle()
+                            }) {
+                                Text("Preview")
+                                    .font(.custom("GillSans-SemiBold", size: 20))
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(
+                                        LinearGradient(
+                                            gradient: Gradient(colors: [
+                                                Color(red: 77/255, green: 143/255, blue: 248/255),
+                                                Color(red: 140/255, green: 140/255, blue: 255/255)
+                                            ]),
+                                            startPoint: .topLeading,
+                                            endPoint: .bottomTrailing
+                                        )
+                                    )
+                                    .cornerRadius(30)
+                            }
+                        }
+                        
+                    }
+                }.frame(height: 520)
+                VStack {
+                    if isTextHidden {
+                        Button(action: {
+                            isTextHidden.toggle()
+                        }) {
+                            Text("Finish preview")
+                                .font(.custom("GillSans-SemiBold", size: 20))
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color(red: 77/255, green: 143/255, blue: 248/255))
+                                .cornerRadius(30)
+                        }
+                    }
+                }.frame(height: 80)
+
             }
         }
     }
