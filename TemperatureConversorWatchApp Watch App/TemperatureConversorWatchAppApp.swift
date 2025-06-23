6aee5fd1846ab1832a4d60fe90b186b8f0b023de
j-peace <tiagopaz.hs@gmail.com>
2025-06-23 18:00:21 -0300

    Add watchOS app target

---
+++ TemperatureConversorWatchApp Watch App/TemperatureConversorWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversorWatchAppApp.swift
+//  TemperatureConversorWatchApp Watch App
+//
+//  Created by Suno on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversorWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
